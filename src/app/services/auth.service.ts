b17d1bb6c1cfccd872115c0a433099f14b7729eb
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-18 17:14:24 -0400

    Comments

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,11 +17,12 @@ export const ANONYMOUS_USER: User = {
 export class AuthService {
 
 // CREATE SOME OBSERVABLES
-    // Right off the bat undefined is placed in the Observable stream
+  // The <User> says that the shape of data that will be placed in the stream will adhere to User Interface or Class
+  // Right off the bat undefined is placed in the stream created by BehaviorSubject
     private subject = new BehaviorSubject<User>(undefined);
 
-    // Stuff from the subject observable stream will find its way into the user$ observable stream provided the stuff
-    // resolves to truthy
+    // Stuff from the subject stream will find its way into the user$ observable stream provided the stuff
+    // resolves to truthy. That is what the filter() is doing ...
     // So, the undefined that we placed in the observable stream will fail the filter defined below and the undefined will NOT
     // be placed in the user$ observable stream.
     user$: Observable<User> = this.subject.asObservable().pipe(filter(user => !!user));
