d2bd2d72ecbfcab951fbdcaa6d6d7c9b5bd7c1bb
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-19 18:19:35 -0400

    Comments - Basically delete the cookies

--- server/logout.route.ts
+++ server/logout.route.ts
@@ -1,14 +1,7 @@
-
-
 import {Request, Response} from 'express';
 
-
-
 export function logout(req: Request, res: Response) {
-
-    res.clearCookie("SESSIONID");
-
-    res.clearCookie("XSRF-TOKEN");
-
+    res.clearCookie("SESSIONID");                           // Delete the cookie
+    res.clearCookie("XSRF-TOKEN");                          // Delete the cookie
     res.sendStatus(200);
 }
