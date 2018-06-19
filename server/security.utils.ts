517d07b2a69d039c85d2b6c2b6f9c497cb8682d2
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-19 10:40:09 -0400

    Comments

--- server/security.utils.ts
+++ server/security.utils.ts
@@ -1,6 +1,3 @@
-
-
-
 import moment = require("moment");
 const util = require('util');
 const crypto = require('crypto');
@@ -13,47 +10,28 @@ import {DbUser} from "./db-user";
 export const randomBytes = util.promisify(crypto.randomBytes);
 
 export const signJwt = util.promisify(jwt.sign);
-
-
 const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
-
 const RSA_PUBLIC_KEY = fs.readFileSync('./demos/public.key');
-
 const SESSION_DURATION = 1000;
 
 
+// Creates the JWT token and shoves the user id in its PAYLOAD in a property called subject:
 export async function createSessionToken(user: DbUser) {
-    return signJwt({},
+    return signJwt({},            
         RSA_PRIVATE_KEY, {
-        algorithm: 'RS256',
+        algorithm: 'RS256',      
         expiresIn: 7200,
         subject: user.id.toString()
     });
 }
 
-
+// Makes sure that the JWT token is kosher and then extracts the payload out of it and returns it to the caller
 export async function decodeJwt(token:string) {
-
     const payload = await jwt.verify(token, RSA_PUBLIC_KEY);
-
     console.log("decoded JWT payload", payload);
-
     return payload;
 }
 
 export async function createCsrfToken() {
     return await randomBytes(32).then(bytes => bytes.toString("hex"));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
