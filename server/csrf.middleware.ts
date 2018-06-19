3ea7fa9b25bddf2dc5800e6563d6af711123cf4d
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-19 18:29:35 -0400

    Comments - Stop processing if x-xsrf-token header does not match token in cookie

--- server/csrf.middleware.ts
+++ server/csrf.middleware.ts
@@ -1,14 +1,9 @@
-
-
-
 import {Request, Response, NextFunction} from 'express';
 
-
-
+// This middleware STOPS processing the Http Request any further and returns status 403 IF the token in the HTTP Header's 
+// x-xsrf-token property is NOT the same as the token inside the cookie.
 export function checkCsrfToken(req: Request,  res: Response,  next: NextFunction) {
-
     const csrfCookie = req.cookies["XSRF-TOKEN"];
-
     const csrfHeader = req.headers['x-xsrf-token'];
 
     if (csrfCookie && csrfHeader && csrfCookie === csrfHeader) {
@@ -17,7 +12,5 @@ export function checkCsrfToken(req: Request,  res: Response,  next: NextFunction
     else {
         res.sendStatus(403);
     }
-
-
 }
 
