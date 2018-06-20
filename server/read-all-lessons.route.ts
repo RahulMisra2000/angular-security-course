7e1cf07d8b0a1e545a5667d4e7c4bd50bb9f92ac
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-20 17:41:56 -0400

    Comments : Always return an Object from WEB API

--- server/read-all-lessons.route.ts
+++ server/read-all-lessons.route.ts
@@ -4,6 +4,10 @@ import {db} from "./database";
 
 export function readAllLessons(req, res) {
 
-    res.status(200).json({lessons:db.readAllLessons()});
+    // REMEMBER:  Always a good idea for WEB APIs to return an object and hence the  {   } below. It should not directly return an array
+    //            The object can of course have any shape ...it could be an array of other objects .... etc.
+    //            In the example below, db.readAllLessons() returns an array so basically 
+    //            here we are returning  { lessons:  [ ... ]   }
+    res.status(200).json( { lessons:db.readAllLessons() } );
 
 }
