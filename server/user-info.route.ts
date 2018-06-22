e59ddc8a5b57becc0d98b2a1a00122b68b756367
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-22 00:30:38 -0400

    Comments

--- server/user-info.route.ts
+++ server/user-info.route.ts
@@ -1,17 +1,15 @@
-
-
 import {Request, Response} from "express";
 import {db} from "./database";
 
 
+// This function adds the user to OUR DATABASE (remember the user is already in Auth0's database)
+// and then returns the user's email address 
 export function userInfo(req:Request, res:Response) {
-
+    // The Http Request Object's user property is filled by the jwt payload by the Middleware
     const userInfo = req.user;
 
     console.log("Checking if user exists", userInfo);
-
     let user = db.findUserByEmail(userInfo.email);
-
     if (!user) {
         user = db.createUser(userInfo.email,userInfo.sub);
     }
