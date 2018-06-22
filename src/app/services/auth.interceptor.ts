0191fe64789ff085707579f9c439b0021585970d
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-22 00:41:29 -0400

    Comments

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -8,26 +8,18 @@ import {Observable} from "rxjs/Observable";
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-
-    intercept(req: HttpRequest<any>,
-              next: HttpHandler): Observable<HttpEvent<any>> {
+    // JUST pulls out the JWT token from inside the localStorage and places it in the Http Header 
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         const idToken = localStorage.getItem("id_token");
-
         if (idToken) {
             const cloned = req.clone({
-                headers: req.headers.set("Authorization",
-                    "Bearer " + idToken)
+                                    headers: req.headers.set("Authorization", "Bearer " + idToken)
             });
-
             return next.handle(cloned);
         }
         else {
             return next.handle(req);
         }
-
-
     }
-
-
 }
