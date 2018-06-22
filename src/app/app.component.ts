ecf3413fe1b79c56ae23619b561eb338b5472d43
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-22 01:25:45 -0400

    Comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,11 @@ export class AppComponent  implements OnInit {
     }
 
     ngOnInit() {
-
+        // This is called here because 
+        // On successful authentication, Auth0 redirects the browser BACK to our Angular Application at a URL (called callback url) 
+        // (that we provided to the Auth0 portal) with the jwt token as a url hash segment
+        // Because our Angular Application is redirected to … it is like starting up Angular Application … so in the Bootstrap 
+        // Component’s ngOnInit we read the hash segment and save the jwt and its expiry in localStorage
         this.auth.retrieveAuthInfoFromUrl();
     }
 
